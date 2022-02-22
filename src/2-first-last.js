d77e27206e15def28ed43daf1a28ea9bd429dd36
anki palviainen <anki.palviainen@gmail.com>
2022-02-22 19:06:00 +0200

    2

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,12 +1,12 @@
 export const firstLast = (items) => {
-  if (items.lenght === 0) {
-    return `No items!`
+  if (items.length === 0) {
+    return 'No items!'
+  } else if (items.length === 1) {
+    return `Only item: ${items[0]}`
+  } else if (items.length > 2 ) {
+    return `First: ${items[0]}, Last: ${items[items.length-1]}`
+  } else {
+    return `First: ${items[0]}, Last: ${items[1]}`
   }
-
-
-if (items.lenght === 1) {
-  
- }
- return `First: ${items[0]}, Last: ${items[1]}`
 }
   
